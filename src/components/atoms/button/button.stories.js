7f0d93f635e588bf3ff11f6e3bdc1d8b5113f6a7
Dominique Börner <dominique@mukosoft.de>
2023-01-26 17:15:44 +0100

    Make storybook prettier

--- src/components/atoms/button/button.stories.js
+++ src/components/atoms/button/button.stories.js
@@ -7,14 +7,16 @@ export default {
 export const Buttons = () => {
   return `
     <div class="flex flex-col">
-        <div>
+        <div class="element-row">
+            <span class="label">Default:</span>
             <neon-button primary label="Button"></neon-button>
             <neon-button primary rounded label="Button"></neon-button>
             <neon-button secondary label="Button"></neon-button>
             <neon-button secondary rounded label="Button"></neon-button>
             <neon-button text label="Button"></neon-button>
         </div>
-        <div>
+        <div class="element-row">
+            <span class="label">Hover:</span>
             <neon-button primary glowing label="Button"></neon-button>
             <neon-button primary glowing rounded label="Button"></neon-button>
             <neon-button secondary glowing label="Button"></neon-button>
